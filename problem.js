aa544bf500f2a6d0515ba3f2550e97c2817aab4f
Justin Nguyen <jbnguyen89@yahoo.com>
2019-01-11 22:38:47 -0700

    fixed the code

--- problem.js
+++ problem.js
@@ -6,7 +6,10 @@ function fizzBuzz(){
     //Else, print the number
     for(i=1;i<=100;i++){
 
-	if(i % 3 === 0){
+	if(i % 15 ===0){
+		console.log("fizzbuzz");
+	}
+	    else if(i % 3 === 0){
             console.log("fizz");
         }
         else if(i % 5 === 0){
